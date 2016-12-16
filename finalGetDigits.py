b7ea8ff66e5a7aeaf5d87cc6887059900f713418
bhargavimadhunala <bhargavi.madhunala1@marist.edu>
2016-12-16 07:29:36 -0500

    Add files via upload
    Hi professor, I tried directly from git hub to upload my files when i am trying from git shell it did not work.

---
+++ finalGetDigits.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy import genfromtxt
+
+def getDataSet():
+
+	dataset = genfromtxt('features.csv', delimiter=' ')
+	y = dataset[:, 0]
+	X = dataset[:, 1:]
+
+	dataset = genfromtxt('features-t.csv', delimiter=' ')
+	y_te = dataset[:, 0]
+	X_te = dataset[:, 1:]
+
+	return X, y, X_te, y_te
